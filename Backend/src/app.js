21c3c83b2c5922ccd3437575e73b732d740001f7
vanshDalal521 <vanshd994@gmail.com>
2026-04-23 11:48:12 +0530

    chore: make project Vercel-ready

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,6 +1,9 @@
 const express = require("express")
 const cookieParser = require("cookie-parser")
 const cors = require("cors")
+const connectToDB = require("./config/database")
+
+connectToDB()
 
 const app = express()
 
--- Frontend/src/features/auth/services/auth.api.js
+++ Frontend/src/features/auth/services/auth.api.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:3000",
+    baseURL: import.meta.env.VITE_API_BASE_URL || "",
     withCredentials: true,
 })
 
